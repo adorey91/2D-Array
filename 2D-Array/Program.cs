864f3d2d1304af0939a99f3a636142546ca5dcaf
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-10-31 15:23:26 -0300

    Initial commit

---
+++ 2D-Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2D Arrays");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
